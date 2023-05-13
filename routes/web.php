a95e147cb8807c53461818087a37cb055c359768
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2023-05-13 19:23:04 +0700

    master

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,5 +14,22 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::controller(DashboardController::class)->group(function () {
+  Route::get('/', 'index')->name('dashboard.index');
+  Route::view('/{name}', 'view')->name('dashboard.view');
+});
+
+Route::controller(PostsController::class)->group(function () {
+  Route::get('/posts', 'index')->name('posts.index');
+  Route::view('/posts/{name}', 'view')->name('posts.view');
+  Route::get('/post/create', 'create')->name('posts.create');
+  Route::post('/post/create', 'store')->name('posts.store');
+  Route::get('/post/edit/{name}', 'edit')->name('posts.edit');
+  Route::put('/post/edit/{name}', 'update')->name('posts.update');
+});
+
+Route::controller(UsersController::class)->group(function () {
+});
 
 require __DIR__ . '/auth.php';
