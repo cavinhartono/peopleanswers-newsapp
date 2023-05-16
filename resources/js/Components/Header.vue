8f5b36c9057bfb963c6e58fc87fd962d8d4d4581
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2023-05-16 21:08:57 +0700

    master

---
+++ resources/js/Components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header class="header">
+        <div class="menu">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <h2 class="title">peopleanswers</h2>
+        <div class="search">
+            <label for="search"><i class="ri-search-2-line"></i></label>
+            <div class="input" id="search">
+                <input type="text" placeholder="What is find..." />
+            </div>
+        </div>
+    </header>
+    <nav class="navigation">
+        <ul class="nav">
+            <li :class="`list`">
+                <a href="{{ route('home') }}" class="link"> features </a>
+            </li>
+            <li :class="`list`">
+                <a href="{{ route('dashboard.style') }}" class="link">
+                    style
+                </a>
+            </li>
+            <li :class="`list`">
+                <a href="{{ route('dashboard.travel') }}" class="link">
+                    travel
+                </a>
+            </li>
+            <li :class="`list`">
+                <a href="{{ route('dashboard.opinion') }}" class="link">
+                    opinion
+                </a>
+            </li>
+        </ul>
+    </nav>
+</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,5 +1,11 @@
 <template>
+    <Head title="Dashboard - Peopleanswers" />
+    <div class="container">
+        <Header />
+    </div>
 </template>
 
 <script setup>
+import Header from "@/Components/Header.vue";
+import { Head } from "@inertiajs/vue3";
 </script>
