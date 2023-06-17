bb041fd4d2ea012a938ce72dc0244925052f622f
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2023-06-17 21:21:41 +0700

    master

--- resources/js/Components/Homepage/MustRead.vue
+++ resources/js/Components/Homepage/MustRead.vue
@@ -4,8 +4,11 @@ import { Link } from "@inertiajs/inertia-vue3";
 </script>
 
 <template>
-    <h2 class="text-2xl font-bold text-black-500">
-        <label>must-reads</label>
-        <i class="ri-arrow-right-up-line"></i>
-    </h2>
+    <div class="relative px-10">
+        <h2 class="text-2xl font-bold text-black-500">
+            <label>must-reads</label>
+            <i class="ri-arrow-right-up-line"></i>
+        </h2>
+        <div class="w-full"></div>
+    </div>
 </template>
