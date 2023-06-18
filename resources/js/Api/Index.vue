5dbe315982e50bc87502b187caa78f9191502c95
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2023-06-18 17:26:19 +0700

    master

---
+++ resources/js/Api/Index.vue
@@ -0,0 +1,8 @@
+<script setup>
+const endpoint =
+    "https://newsapi.org/v2/top-headlines?country=id&apiKey=efb8274e78c24138ab578fb98d173181";
+
+fetch(endpoint)
+    .then((article) => article.json())
+    .then((result) => result.article);
+</script>
--- resources/js/Pages/Homepage.vue
+++ resources/js/Pages/Homepage.vue
@@ -3,6 +3,13 @@ import Header from "@/Components/Header.vue";
 import Footer from "@/Components/Footer.vue";
 import Homepage from "@/Components/Homepage/Homepage.vue";
 import MustReads from "@/Components/Homepage/MustRead.vue";
+
+const endpoint =
+    "https://newsapi.org/v2/top-headlines?country=id&apiKey=efb8274e78c24138ab578fb98d173181";
+
+fetch(endpoint)
+    .then((articles) => articles.json())
+    .then((result) => console.log(result.articles));
 </script>
 
 <template>
