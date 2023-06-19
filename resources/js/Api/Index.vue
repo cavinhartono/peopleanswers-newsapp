8435da10c36b653f3c3a75c8174b149e86c24c39
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2023-06-19 19:26:39 +0700

    master

--- resources/js/Api/Index.vue
+++ resources/js/Api/Index.vue
@@ -1,8 +1,1 @@
-<script setup>
-const endpoint =
-    "https://newsapi.org/v2/top-headlines?country=id&apiKey=efb8274e78c24138ab578fb98d173181";
-
-fetch(endpoint)
-    .then((article) => article.json())
-    .then((result) => console.log(result.article));
-</script>
+<script setup></script>
--- resources/js/Pages/Homepage.vue
+++ resources/js/Pages/Homepage.vue
@@ -2,21 +2,29 @@
 import Header from "@/Components/Header.vue";
 import Footer from "@/Components/Footer.vue";
 import Homepage from "@/Components/Homepage/Homepage.vue";
-import MustReads from "@/Components/Homepage/MustRead.vue";
+import MustRead from "@/Components/Homepage/MustRead.vue";
 
 const endpoint =
     "https://newsapi.org/v2/top-headlines?country=id&apiKey=efb8274e78c24138ab578fb98d173181";
 
 fetch(endpoint)
     .then((articles) => articles.json())
-    .then((result) => console.log(result.articles));
+    .then((result) => {
+        result.articles.map((item) => {
+            const createdBy = item.author;
+            const createdAt = item.publishedAt;
+            const title = item.title;
+            const url = item.url;
+            const source = item.source.name;
+        });
+    });
 </script>
 
 <template>
     <div class="container mx-auto">
         <Header />
         <Homepage />
-        <MustReads />
+        <MustRead />
         <Footer />
     </div>
 </template>
